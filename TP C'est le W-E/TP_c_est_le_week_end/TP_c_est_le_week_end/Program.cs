d3aee98d77440ab7cf999eaf96d33d68199fc1ca
SimonCambier <belgegwada@outlook.com>
2015-05-26 17:08:06 +0200

    TP "C'est le Week-End"
    Ajout  de la solution Visual Studio (vide)

---
+++ TP C'est le W-E/TP_c_est_le_week_end/TP_c_est_le_week_end/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_c_est_le_week_end
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
